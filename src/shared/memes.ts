29c4c06eeac03ef598708ffe14fd4535f6d4562e
Les Holikov <sanyagolikov97@gmail.com>
2025-08-16 17:25:18 +0300

    more memes

--- src/shared/memes.ts
+++ src/shared/memes.ts
@@ -1,7 +1,10 @@
 const memesStore = [
-  "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.meme-arsenal.com%2Fcreate%2Ftemplate%2F762906&psig=AOvVaw0uiuwESGDkKD_Kn9wF--WL&ust=1755439703408000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCODB8NjAj48DFQAAAAAdAAAAABAL",
-  "https://www.google.com/url?sa=i&url=https%3A%2F%2Fsteamcommunity.com%2Fsharedfiles%2Ffiledetails%2F%3Fid%3D1514889949&psig=AOvVaw0uiuwESGDkKD_Kn9wF--WL&ust=1755439703408000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCODB8NjAj48DFQAAAAAdAAAAABAU",
-  "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.reddit.com%2Fr%2FLeagueOfMemes%2Fcomments%2F1grezdk%2Fthank_you_dota_2%2F&psig=AOvVaw26Vbex6XvH_IUJSBktT1-d&ust=1755439958046000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCKCDi83Bj48DFQAAAAAdAAAAABAL",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGscHYj8VdN2NzbM4dzyJa2ASiaSp5lARQVQ&s",
+  "https://i.pinimg.com/736x/fe/8a/66/fe8a665354b199d9983082aea5a34be0.jpg",
+  "https://preview.redd.it/thank-you-dota-2-v0-q2m94sp6kx0e1.jpeg?width=320&crop=smart&auto=webp&s=fa656802fcda0d62bae6dcc3e5c22877acc8e618",
+  "https://www.clipartmax.com/png/middle/70-700395_actually-thats-the-title-not-the-watermark-dota-2-mmr-meme.png",
+  "https://i.pinimg.com/736x/b5/2a/da/b52ada98c3ad5761614be69354d5666a.jpg",
+  "https://i.pinimg.com/736x/1d/73/26/1d7326ec42384f0779315f79ba8a7da1.jpg",
 ];
 
 export default memesStore;
