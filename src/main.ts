3e534481ab4b74692256603b034261bb995a7e99
Les Holikov <sanyagolikov97@gmail.com>
2025-08-16 16:33:56 +0300

    Remove server

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,12 @@
 import "dotenv/config";
 import { BeerDotaBot } from "./bot/BeerDotaBot.js";
-import { ExpressServer } from "./server/ExpressServer.js";
-
-const token = process.env.BOT_TOKEN;
-const port = Number(process.env.PORT || 3000);
-const webhookUrl = process.env.WEBHOOK_URL || "";
-const webhookPath = process.env.WEBHOOK_PATH || "/bot";
-
-if (!token) throw new Error("BOT_TOKEN is required");
 
 async function main() {
-  const bot = await BeerDotaBot.create(token!);
+  const token = process.env.BOT_TOKEN;
+  if (!token) throw new Error("BOT_TOKEN is required");
 
-  if (webhookUrl) {
-    const server = new ExpressServer(bot, port);
-    await server.start(webhookUrl, webhookPath);
-  } else {
-    await bot.startPolling();
-  }
+  const bot = await BeerDotaBot.create(token);
+  await bot.startPolling();
 }
 
 main().catch(console.error);
